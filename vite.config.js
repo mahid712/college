0bc6cce7f90c356949cfd18007438b7c28453ec2
mahid_sheik <mahidm712@gmail.com>
2026-08-17 23:46:42 +0530

    trigger new deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Must match your repository name exactly
+  base: '/college/', // Must match your repository name exactly
 })
