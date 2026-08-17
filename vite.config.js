132cde9e2ab6a163c3f7797d41d7fbba41addd7b
mahid_sheik <mahidm712@gmail.com>
2026-08-17 23:30:23 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/college/', // Must match your repository name exactly
+  base: '/', // Must match your repository name exactly
 })
