f560ebe0ca6ef51b53735d0bdaa4af9cc219fc3f
mahid_sheik <mahidm712@gmail.com>
2026-08-18 14:29:52 +0530

    Fix import case for QuickLinks component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import QuickLinks from './components/QuickLinks';
+import QuickLinks from './components/Quicklinks';
 import Footer from './components/Footer';
 import Academics from './components/Academics';
 import Research from './components/Research';
