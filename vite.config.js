c2ba0a57ecd5fea06b275a37dcb4336a09f1bf3f
mahid_sheik <mahidm712@gmail.com>
2026-08-21 20:17:35 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' for Vercel or custom domains
+  base: '/', // Use '/' for Vercel
 })
