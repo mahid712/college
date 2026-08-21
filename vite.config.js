5dc09679fc684c403caf468e3de9750933279aa8
mahid_sheik <mahidm712@gmail.com>
2026-08-21 20:11:44 +0530

    lets do it

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/college/', // Must match your repository name exactly
+  base: '/', // Use '/' for Vercel or custom domains
 })
